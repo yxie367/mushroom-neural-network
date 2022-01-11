768497b489103c3a05c1e15addce85af95325cf9
yxie367 <yxie367@gatech.edu>
2022-01-11 18:51:12 -0500

    Updated data_prep.py

--- source/data_prep.py
+++ source/data_prep.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
 #Reading data file
-df = pd.read_csv('agaricus-lepiota.data')
+df = pd.read_csv('C:\\Users\\xieya\\Documents\\GitHub\\mushroom-neural-network\\source\\agaricus-lepiota.data')
 
 features = df.iloc[:,0:]
 #One hot encoding
@@ -12,7 +12,7 @@ features = pd.get_dummies(features).astype(float)
 features.head()
 
 #Output the encoded file
-features.to_csv('hot_encoded.txt', index=False,header=None) 
+features.to_csv('hot_encoded.txt', index=False,header=None)
 
 #Split the data into training(70), validation(15) and test(15) datasets
 train, validate, test = np.split(features.sample(frac=1), [int(.7*len(features)), int(.85*len(features))])
@@ -23,6 +23,6 @@ validate = validate.astype(int)
 test = test.astype(int)
 
 #Save the datasets
-train.to_csv('training.txt', index=False,header=None,) 
-validate.to_csv('val.txt', index=False,header=None,) 
-test.to_csv('testing.txt', index=False,header=None,)
+train.to_csv('C:\\Users\\xieya\\Documents\\GitHub\\mushroom-neural-network\\training.txt', index=False,header=None,)
+validate.to_csv('C:\\Users\\xieya\\Documents\\GitHub\\mushroom-neural-network\\val.txt', index=False,header=None,)
+test.to_csv('C:\\Users\\xieya\\Documents\\GitHub\\mushroom-neural-network\\testing.txt', index=False,header=None,)
